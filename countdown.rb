7636b8f2c436d925c4015cb67c80528d989c66f3
Changmin Lim <chang.lim92@gmail.com>
2018-05-25 05:02:13 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,8 +2,11 @@
 
 def countdown(int)
   while int > 0 
-  puts "#{int} SECOND (S)!"
+  puts "#{int} SECOND(S)!"
   int -= 1 
 end 
 return "HAPPY NEW YEAR!"  
 end
+
+def countdown_with_sleep(int) 
+  
